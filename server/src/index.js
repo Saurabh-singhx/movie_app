83f2afc68e9be8f89f83273e3c803893a5e05c2c
saurabh kumar <saurabh4442kumar@gmail.com>
2026-01-11 19:08:53 +0530

    deployment config

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT;
 
 app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({limit:"10mb",extended:true}));
@@ -18,7 +18,8 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",process.env.FRONTEND_URL],
+
     credentials:true,
 }));
 
